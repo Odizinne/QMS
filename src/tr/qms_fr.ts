aac3ced90a07eb9c19d6d0dea41b790d14677034
Advitil <76661984+Advitil@users.noreply.github.com>
2024-08-08 17:09:21 +0200

    completed french translation file
    * completed french translation file

--- src/tr/qms_fr.ts
+++ src/tr/qms_fr.ts
@@ -1,55 +1,55 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
-  <context>
+<TS version="2.1" language="fr_FR">
+<context>
     <name>MainWindow</name>
     <message>
-      <location filename="..\ui\design.ui" line="0" />
-      <source>QMS - Settings</source>
-      <translation type="unfinished" />
+        <location filename="../ui/design.ui" line="0"/>
+        <source>QMS - Settings</source>
+        <translation>QMS - Paramètres</translation>
     </message>
     <message>
-      <location filename="..\ui\design.ui" line="0" />
-      <source>General settings</source>
-      <translation type="unfinished" />
+        <location filename="../ui/design.ui" line="0"/>
+        <source>General settings</source>
+        <translation>Paramètres généraux</translation>
     </message>
     <message>
-      <location filename="..\ui\design.ui" line="0" />
-      <source>Send ON/OFF signal to:</source>
-      <translation type="unfinished" />
+        <location filename="../ui/design.ui" line="0"/>
+        <source>Send ON/OFF signal to:</source>
+        <translation>Envoie d&apos;un signal ON/OFF à :</translation>
     </message>
     <message>
-      <location filename="..\ui\design.ui" line="0" />
-      <source>Run at startup</source>
-      <translation type="unfinished" />
+        <location filename="../ui/design.ui" line="0"/>
+        <source>Run at startup</source>
+        <translation>Lancer au démarrage</translation>
     </message>
     <message>
-      <location filename="..\ui\design.ui" line="0" />
-      <source>Rescan monitors</source>
-      <translation type="unfinished" />
+        <location filename="../ui/design.ui" line="0"/>
+        <source>Rescan monitors</source>
+        <translation>Rechercher des écrans</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>QMS</name>
     <message>
-      <location filename="..\qms.py" line="122" />
-      <source>Enable secondary monitors</source>
-      <translation type="unfinished" />
+        <location filename="../qms.py" line="122"/>
+        <source>Enable secondary monitors</source>
+        <translation>Activer les écrans externes</translation>
     </message>
     <message>
-      <location filename="..\qms.py" line="124" />
-      <source>Disable secondary monitors</source>
-      <translation type="unfinished" />
+        <location filename="../qms.py" line="124"/>
+        <source>Disable secondary monitors</source>
+        <translation>Désactiver les écrans externes</translation>
     </message>
     <message>
-      <location filename="..\qms.py" line="129" />
-      <source>Settings</source>
-      <translation type="unfinished" />
+        <location filename="../qms.py" line="129"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
     </message>
     <message>
-      <location filename="..\qms.py" line="132" />
-      <source>Exit</source>
-      <translation type="unfinished" />
+        <location filename="../qms.py" line="132"/>
+        <source>Exit</source>
+        <translation>Quitter</translation>
     </message>
-  </context>
+</context>
 </TS>
